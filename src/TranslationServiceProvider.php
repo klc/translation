ba0167d8b77a422db82d04fb291f34153e9f973b
klc <m_kilic@outlook.com.tr>
2021-09-18 12:16:51 +0300

    fixed auto publish

--- src/TranslationServiceProvider.php
+++ src/TranslationServiceProvider.php
@@ -9,12 +9,12 @@ class TranslationServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        if(App::runningInConsole()) {
-            $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-             $this->publishes([
-                 __DIR__.'/../database/migrations' => $this->app->databasePath('migrations'),
-                 __DIR__.'/../app/Models' => $this->app->basePath('app/Models')
-             ]);
+        if (App::runningInConsole()) {
+            $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+            $this->publishes([
+                __DIR__ . '/../database/migrations' => $this->app->databasePath('migrations'),
+                __DIR__ . '/../app/Models' => $this->app->basePath('app/Models')
+            ], 'klc-translation');
         }
     }
 }
